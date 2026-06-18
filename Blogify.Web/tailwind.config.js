0fb1658d2b63157c46ff03656b1c82bf4f915a4e
Aerys-cmd <recepobut72@gmail.com>
2026-06-18 15:55:12 +0300

    feat: blog themes visual improvements

--- Blogify.Web/tailwind.config.js
+++ Blogify.Web/tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './Areas/Blog/Themes/{Minimal,Aurora}/**/*.cshtml',
+    './Areas/Blog/Pages/**/*.cshtml',
+    './Areas/Blog/Themes/**/*.cshtml',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Source Sans 3"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        serif: ['Georgia', 'Cambria', '"Times New Roman"', 'serif'],
+        mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Consolas', 'monospace'],
+      },
+      colors: {
+        ink: '#17191F',
+        canvas: '#F6F7F5',
+        surface: '#FFFFFF',
+        subtle: '#ECEEEA',
+        line: '#D8DCD6',
+        muted: '#626970',
+        brand: '#B83A18',
+        bright: '#F06432',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
